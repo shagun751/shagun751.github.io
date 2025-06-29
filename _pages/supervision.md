5bfe179e1110b8b2465a6d3484271a6088fd0824
Shagun Agarwal <shagun.1994@gmail.com>
2025-06-29 02:38:09 +0200

    Added conference talks and TU Delft Master thesis supervision.

---
+++ _pages/supervision.md
@@ -0,0 +1,23 @@
+---
+layout: archive
+title: "Supervision"
+permalink: /supervision/
+author_profile: true
+---
+
+
+## Master Thesis - TU Delft
+
+1. Chen Liang (ongoing). 
+1. Kevin Kevin Jonathan (ongoing). Neural Enhancement of Schapery-Prony Viscoelastic Theory
+1. Rafael R. Rutgers (2025). [Surrogate Deep Learning Models for response of Very Large Floating Structures](https://resolver.tudelft.nl/uuid:add9669d-9bbc-4c08-b65c-2766f5f5c081)
+1. Hidde Drost (2025). [Wave run-up on monopiles](https://resolver.tudelft.nl/uuid:edadfaaa-6970-45ec-b599-16f52b027862)
+1. Kareem El Sayed (2024). [Control of Flexible Floating Islands](https://resolver.tudelft.nl/uuid:b5ac6396-aff4-4cb9-84f5-bb761703f483)
+1. Jesper T Bryan (2024). [Daisy-chained floating wind turbines](https://resolver.tudelft.nl/uuid:b10c6d78-89b8-4940-bd57-b12799d1fd94)
+1. Khalil Akchikchi (2024). [Ship-induced waves interacting with floating membranes](https://resolver.tudelft.nl/uuid:9a132fa5-c3c5-42b3-af6a-ea8e05425e46)
+1. Nebo Wemekamp (2023). [Systematic approach to find a suitable floating PV-structure on a given location](http://resolver.tudelft.nl/uuid:98cffd61-49ef-44ee-a8af-573bcf17e189)
+1. Philipp Tiwald (2023). [Wave Effects on Power Mismatch Losses in Offshore Floating PV](https://repository.tudelft.nl/islandora/object/uuid%3A08cb4b5e-3801-46c4-81a3-5427d09d1a8a)
+
+
+
+
--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,32 @@
 var addressPoints = [
   [
-    "Conference Proceeding talk 3 on Relevant Topic in Your Field<br />Testing Institute of America 2014 Annual Conference; Los Angeles, CA, USA",
-    34.0536909,
-    -118.242766
+    "Interaction of Fixed Cylinder With Waves Through Weakly Coupled FNPT and Lagrangian Navier-Stokes<br />OMAE 2019; Glasgow, Scotland, UK",
+    55.861155,
+    -4.2501687
   ],
   [
-    "Modelling Wave Interaction with Porous Structures Using Boussinesq Equations<br />ICSR, IIT Madras; Chennai, India",
+    "Finite-element Model for Geometric Nonlinearity and Dynamic Stiffness of Synthetic Mooring Lines<br />ISOPE 2025; Goyang, South Korea",
+    37.6581862,
+    126.8319452
+  ],
+  [
+    "Fluid-fluid coupling between mesh and particle-based models for wave-structure interaction<br />EURODYN 2023; Delft, The Netherlands",
+    51.9994572,
+    4.3627245
+  ],
+  [
+    "TDC for finite elastic deformation in submerged mooring lines exposed to dynamic loading<br />WCCM 2024; Vancouver, Canada",
+    49.2608724,
+    -123.113952
+  ],
+  [
+    "Modelling Wave Interaction with Porous Structures Using Boussinesq Equations<br />ICOE 2018; Chennai, India",
     13.0836939,
     80.270186
   ],
   [
-    "Talk 2 on Relevant Topic in Your Field<br />London School of Testing; London, UK",
-    51.4893335,
-    -0.1440551
-  ],
-  [
-    "Tutorial 1 on Relevant Topic in Your Field<br />UC-Berkeley Institute for Testing Science; Berkeley, CA, USA",
-    37.8708393,
-    -122.272863
+    "Hydro-visco-elastic analysis of floating membranes using a monolithic finite-element model<br />MARINE 2023; Madrid, Spain",
+    40.4167047,
+    -3.7035825
   ]
 ];
